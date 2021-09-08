c7bc8a68fb4b59e8be77767e245bbdb6899a4de3
moiarahis <torovin.p@yandex.ru>
2021-09-08 16:01:14 +0300

    1

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long double x, Eps, Tsum, diff, N, Slog, sum;
+    int Znak;
+    
+    Znak = -1;
+    Eps = (1 / 2.71828);
+
+    for () {
+
+    }
+
+}
